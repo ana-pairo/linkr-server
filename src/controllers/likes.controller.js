293e3461b3819e092196f35f6505b2fcc6f8b4a0
gabao55 <gabriel.s.rosin@gmail.com>
2022-10-18 21:08:51 -0300

    Implements likes listing for specific post (still have to test)

--- src/controllers/likes.controller.js
+++ src/controllers/likes.controller.js
@@ -9,7 +9,27 @@ async function unlikePost (req, res) {
 }
 
 async function listPostLikes (req, res) {
-    
+    const { postId } = req.params;
+
+    try {
+        const postLikes = (await connection.query(
+            `
+                SELECT 
+                    COUNT(likes.id) AS "totalLikes", users.name AS username 
+                FROM 
+                    likes
+                JOIN users ON likes."userId" = users.id
+                WHERE likes."postId" = $1
+                GROUP BY users.name;
+            `,
+            [postId]
+        )).rows;
+
+        res.status(200).send(postLikes);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
 }
 
 export { likePost, unlikePost, listPostLikes };
