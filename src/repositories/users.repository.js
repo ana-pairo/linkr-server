b8f1a64285d84a264043a24fc8b17d5538d35c13
igorFrotte <igorfrotte@gmail.com>
2022-10-20 13:41:40 -0300

    ListTimeLine Ready

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -24,4 +24,20 @@ function getPostsByUser (userId) {
     );
 }
 
-export { getUserById, getPostsByUser };
+function getAllPosts () {
+    return connection.query(
+        `
+            SELECT               
+                posts.*, users.username, users.picture as "userPhoto", COUNT(likes.id) AS likes
+            FROM
+                posts_trends
+            JOIN posts ON posts_trends."postId" = posts.id
+            JOIN users ON posts."userId" = users.id
+            LEFT JOIN likes ON likes."postId" = posts.id
+            GROUP BY posts.id, users.username, users.picture
+            ORDER BY id DESC LIMIT 20;
+        `
+    );
+}
+
+export { getUserById, getPostsByUser, getAllPosts };
