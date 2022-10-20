1d1a74cdb93915508e86a4e2cb589e9f9281d866
gabao55 <gabriel.s.rosin@gmail.com>
2022-10-19 21:21:15 -0300

    1 - Implements functionality for getting all hashtags related to a post; 2 - Implements functionality for querying posts by hashtag

--- src/controllers/trends.controller.js
+++ src/controllers/trends.controller.js
@@ -1,4 +1,4 @@
-import { getTrends } from "../repositories/trends.repository.js";
+import { getTrends, getHashtagId, getTrendPostsByTrendId, getPostTrendsByPostId } from "../repositories/trends.repository.js";
 
 async function listTrends (req, res) {
     try {
@@ -10,7 +10,36 @@ async function listTrends (req, res) {
     }
 }
 
+async function listTrendPosts (req, res) {
+    const { hashtag } = req.params;
+
+    try {
+        const trend = (await getHashtagId(hashtag)).rows;
+        if (!trend.length) return res.status(404).send('Trend not found');
+        console.log(trend)
+
+        const trendPosts = (await getTrendPostsByTrendId(trend[0].id)).rows;
+
+        return res.status(200).send(trendPosts);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
+async function listPostTrends (req, res) {
+    const { postId } = res.locals;
+
+    try {
+        const postTrends = (await getPostTrendsByPostId(postId)).rows;
+
+        return res.status(200).send(postTrends);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
 export {
     listTrends,
-
+    listTrendPosts,
+    listPostTrends
 }
--- src/repositories/likes.repository.js
+++ src/repositories/likes.repository.js
@@ -52,12 +52,12 @@ function listLikes (postId) {
     return connection.query(
         `
             SELECT 
-                users.name AS username 
+                users.username 
             FROM 
                 likes
             JOIN users ON likes."userId" = users.id
             WHERE likes."postId" = $1
-            GROUP BY users.name;
+            GROUP BY users.username;
         `,
         [postId]
     );
