3421fb283a4519872f28d19111473ce962963bb5
Igor Frotté <107075525+igorFrotte@users.noreply.github.com>
2022-10-28 13:11:28 -0300

    Update users.repository.js

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -14,7 +14,8 @@ function getPostsByUser(userId) {
       JOIN users ON posts."userId" = users.id
         LEFT JOIN likes ON likes."postId" = posts.id
       WHERE users.id = $1
-      GROUP BY posts.id, users.username, users.picture;
+      GROUP BY posts.id, users.username, users.picture
+      ORDER BY posts.id DESC;
     `,
     [userId]
   );
